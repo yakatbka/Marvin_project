811afe352a14e507ef68599270c06ea6a8b925f1
yakatbka <yakatbka@gmail.com>
2018-12-10 15:28:48 +0200

    php

---
+++ php-in-html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <title>Title</title>
+      </head>
+<body>
+<h1>Title</h1>
+<p>
+    <?php
+        echo date('d.m.y h:i:s')
+    ?>
+</p>
+    <p>
+        <?php
+            echo 'Hello, world!';
+        ?>
+    </p>
+</body>
+</html>
